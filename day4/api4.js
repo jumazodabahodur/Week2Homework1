164ce19d5c7918ba66491ef1a2a7f5f3478bc83b
jumazodabahodur <jumazodabahodurr@gmail.com>
2026-02-24 14:10:46 +0500

    24.02.2026

---
+++ day4/api4.js
@@ -0,0 +1,54 @@
+import { ShowUser } from "./app4.js";
+const API = "http://localhost:3001/data";
+
+export async function GetUser() {
+    try {
+        const response = await axios.get(API);
+        ShowUser(response.data);
+    } catch(e) { 
+        console.log(e); 
+    }
+}
+export async function DeleteUser(id) {
+    try { await axios.delete(`${API}/${id}`); } 
+    catch(e){ console.log(e); }
+}
+
+export async function PostUser(obj) {
+    try { await axios.post(API, obj); } 
+    catch(e){ console.log(e); }
+}
+
+export async function EditUserApi(obj, id) {
+    try { await axios.put(`${API}/${id}`, obj); } 
+    catch(e){ console.log(e); }
+}
+
+
+export function FilterByStatus(status) {
+    axios.get(API).then(({data}) => {
+        const filtered = status ? data.filter(u => u.status === status) : data;
+        ShowUser(filtered);
+    });
+}
+
+export function FilterByCategory(category) {
+    axios.get(API).then(({data}) => {
+        const filtered = category ? data.filter(u => u.category === category) : data;
+        ShowUser(filtered);
+    });
+}
+
+const search = document.querySelector(".search");
+
+search.oninput = async () => {
+    const value = search.value.trim();
+
+    try {
+        const response = await axios.get(API, value ? { params: { name_like: value } } : {});
+        ShowUser(response.data); 
+    } catch (error) {
+        console.error(error);
+    }
+};
+GetUser()
